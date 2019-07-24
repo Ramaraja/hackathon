81036589ef78c251d892c4d4c29f5f194523d53f
jai <jairaj.jaisankar@genesys.com>
2019-07-24 10:40:13 +0530

    Added non essential file api's

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 import pandas as pd
 import filesystem_lru
 from application_integrity import IntegrityChecker
+import non_essential_files
 
 app = Flask(__name__)
 #workspace = "/Dinesh/Genesys/designer/workspace/"
-workspace = "/ofs/"
+
 
 
 @app.route("/")
@@ -32,6 +33,31 @@ def app_integrity(ccid):
     data = ic.validate_tenant_apps()
     data = json.dumps(data, indent=2)
     return data
+
+@app.route("/nonessential/<ccid>")
+def app_nonessential(ccid):
+    nonEssentialJson = non_essential_files.non_essential_json(workspace, ccid)
+    data = json.dumps(nonEssentialJson, indent=2)
+    return data
+
+@app.route("/nonessentialcount/<ccid>")
+def app_nonessentialcount(ccid):
+    nonEssentialJson = non_essential_files.non_essential_count(workspace, ccid)
+    data = json.dumps(nonEssentialJson, indent=2)
+    return data
+
+@app.route("/nonessentialfiles/<ccid>")
+def app_nonessentialfile(ccid):
+    nonEssentialJson = non_essential_files.list_non_essential_files(workspace, ccid)
+    data = json.dumps(nonEssentialJson, indent=2)
+    return data
+
+@app.route("/essentialfiles/<ccid>")
+def app_essentialfile(ccid):
+    nonEssentialJson = non_essential_files.list_essential_files(workspace, ccid)
+    data = json.dumps(nonEssentialJson, indent=2)
+    return data
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
